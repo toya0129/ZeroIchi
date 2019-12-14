a53640559b5b615ddd54e129ebd8fc3cce961244
toya0129 <citu15059@g.nihon-u.ac.jp>
2019-12-14 19:03:56 +0900

    add game system and plane ui

--- Assets/Scripts/GameScene/PlayerController.cs
+++ Assets/Scripts/GameScene/PlayerController.cs
@@ -1,36 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private GameController gameController;
+	[SerializeField]
+	private CameraMove cameraMove;
 
     [SerializeField]
     private GameObject ball;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+	private Coroutine power_coroutine;
+	private float power;
+	private const float max_velocity;
 
-    }
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		power_coroutine = StartCoroutine(PowerGauge());
+	}
 
     // Update is called once per frame
     void Update()
     {
+	}
 
-    }
-
-    public void BallShoot()
+	public void BallShoot()
     {
-        gameController.CoroutineStop();
-        float power = GameController.Power * 100;
-        ball.GetComponent<Rigidbody>().AddForce(ball.transform.right * power);
-    }
+		StopCoroutine(power_coroutine);
+		ball.GetComponent<Rigidbody>().velocity = new Vector3(-power, 0, 0);
+
+		cameraMove.SetTarget(ball.transform);
+	}
     public void FloorSweep()
     {
-
+        
     }
+
+    private void FixedVelocity()
+	{
+		Vector3 velocity = ball.GetComponent<Rigidbody>().velocity;
+	}
+
+	private IEnumerator PowerGauge()
+	{
+		bool trigger = true;
+
+		while (true)
+		{
+			if (trigger)
+			{
+				power += 0.1f;
+			}
+			else
+			{
+				power -= 0.1f;
+			}
+
+			if (power > 1)
+			{
+				trigger = false;
+			}
+			else if (power < 0)
+			{
+				trigger = true;
+			}
+			yield return new WaitForSeconds(0.05f);
+		}
+		yield break;
+	}
+
+	#region Getter and Setter
+    public float Power
+	{
+		get { return power; }
+	}
+	#endregion
 }
